340b177514aed458da7c74d94981945e14d20da0
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-04-06 19:33:29 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
