67bb28ad4f5e0394675d8c5d64e8d3ab4d76489b
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-04-08 03:00:08 +0600

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
-import Bookmarks from './components/Bookmarks/Bookmarks';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
